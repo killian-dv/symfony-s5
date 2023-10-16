7e189b8e6c15203699a712f73e034a08ba54a0c3
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-10-16 16:09:31 +0200

    feat: jwt for api

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,11 +10,11 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/login', name: 'app_login', methods: ['POST'])]
-    public function login(#[CurrentUser] $user = null): Response
-    {
-        return $this->json([
-            'user' => $user ? $user->getId() : null,
-        ]);
-    }
+//    #[Route('/login', name: 'app_login', methods: ['POST'])]
+//    public function login(#[CurrentUser] $user = null): Response
+//    {
+//        return $this->json([
+//            'user' => $user ? $user->getId() : null,
+//        ]);
+//    }
 }
